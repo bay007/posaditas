af18d26047c880662cdf98385b3f443feb9edc38
Egaliciar <tnt_code@hotmail.com>
2017-12-01 21:08:46 -0600

    Generando las vistas

--- posadas/views.py
+++ posadas/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from django_filters import rest_framework as filters
+from posadas.models import Posada
+from posadas.serializers import PosadaSerializer
 
 # Create your views here.
+
+
+class ListGenericPosada(generics.ListCreateAPIView):
+    queryset = Posada.objects.all()
+    serializer_class = PosadaSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+
+
+class DetailGenericPosada(generics.RetrieveUpdateAPIView):
+    queryset = Posada.objects.all()
+    serializer_class = PosadaSerializer
